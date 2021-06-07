cbfe9fb10290d1464d6b622936fb75b05b652205
Steven Lin <steven_lx@outlook.com>
2021-06-07 13:28:11 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng12-cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Moutain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking',
+      content: 'I did some biking today',
+    },
+  ];
 }
